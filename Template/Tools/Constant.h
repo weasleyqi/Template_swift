4eddf7da4aff5741fde9c75c200de4a7dec4d2d6
weasley <weasleyqi@gmail.com>
2016-07-12 13:58:11 +0800

    #2
    Multi-target

---
+++ Template/Tools/Constant.h
@@ -0,0 +1,18 @@
+//
+//  Constant.h
+//  Template
+//
+//  Created by Weasley Qi on 16/7/12.
+//  Copyright © 2016年 Weasley Qi. All rights reserved.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+#ifdef Template_UAT
+
+#elif Template
+
+#endif
+
+#endif /* Constant_h */
